bd7352b1aaa84e4c96abc96552a5db749b6a1861
Esteban <acevedoesteban999@gmail.com>
2025-11-12 15:21:26 -0500

    .

--- static/src/components/catalogComponent/catalogComponent.js
+++ static/src/components/catalogComponent/catalogComponent.js
@@ -1,8 +1,10 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { Component , onWillStart , onMounted , useState } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { loadCSS , loadJS } from "@web/core/assets";
+import { useService } from "@web/core/utils/hooks";
+
 
 export class CatalogComponent extends Component {
     static template = "e_sublimation.CatalogComponent";
@@ -10,14 +12,36 @@ export class CatalogComponent extends Component {
     static props = {};
 
     setup() {
+      this.state = useState({'catalogs':[]});
+      this.orm = useService('orm');
+
       onWillStart(async () => {
-        await loadCSS('https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/dist/css/splide.min.css');
-        await loadJS('https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/dist/js/splide.min.js')
+        await this.getData();
+        await loadCSS('https://cdn.jsdelivr.net/npm/swiper@12/swiper-bundle.min.css');
+        await loadJS('https://cdn.jsdelivr.net/npm/swiper@12/swiper-bundle.min.js')
       });
+      
       onMounted(() => {
-          
+        this.swiper = new Swiper('.swiper', {
+          loop: this.state.catalogs.length > 1,
+          speed: 400,
+          spaceBetween: 100,
+          pagination: { el: '.swiper-pagination' },
+          navigation: { nextEl: '.swiper-button-next', prevEl: '.swiper-button-prev' },
+          scrollbar: { el: '.swiper-scrollbar' },
+        });
+
       });
 
+    }
+
+    async getData(){
+      this.state.catalogs = await this.orm.searchRead(
+        'e_sublimation.category',
+        [],
+        ['name','id']
+      )
+       
     }
 }
 
