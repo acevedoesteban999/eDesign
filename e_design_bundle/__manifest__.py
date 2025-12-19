eae1eedbd41e01995eaf4456ae2c4465877c757d
acevedoesteban999 <acevedoesteban999@gmail.com>
2025-12-19 10:48:23 -0500

    [UPD] e_design_bundle: Add search in apps by eDesign [UPD] e_design_bundle: Add ref e_sale_design and e_account_design

--- e_design_bundle/__manifest__.py
+++ e_design_bundle/__manifest__.py
@@ -1,16 +1,18 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'edesignBundle',
-    'version': '18.0.0.0',
+    'version': '18.1.0.0',
     'summary': """ eDesign Bundle for all eDesign addons""",
     'author': 'acevedoesteban999@gmail.com',
     'website': 'https://github.com/acevedoesteban999/eDesign',
     'category': '',
-    'depends': ['e_design','e_pos_mrp','e_pos_mrp_design','e_website_design' ],
+    'depends': ['base','e_design','e_pos_mrp','e_pos_mrp_design','e_website_design','e_sale_design','e_account_design' ],
     'images': [
         'static/description/banner.png',
     ],
-    'data': [],
+    "data": [
+        "views/base_views.xml"
+    ],
     'application': False,
     'installable': True,
     'auto_install': False,
