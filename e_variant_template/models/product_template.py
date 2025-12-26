979b4fb282fdbee79c4d461607d7e40259fbfc73
acevedoesteban999 <acevedoesteban999@gmail.com>
2025-12-26 13:09:34 -0500

    [UPD]

--- e_variant_template/models/product_template.py
+++ e_variant_template/models/product_template.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api, _
+from odoo import models, fields, api, _ , Command
 from odoo.exceptions import UserError, ValidationError
 
 
@@ -15,4 +15,22 @@ class ProductTemplate(models.Model):
     
     @api.onchange('product_variant_template')
     def onchange_product_variant_template(self):
-        pass
+        if self.has_variant_template and self.product_variant_template:
+            for line in self.product_variant_template.attribute_line_ids.filtered_domain([('attribute_id.create_variant','=','always')]):
+                attr = line.attribute_id
+                line_values = line.value_ids.filtered_domain([('attribute_id','=',line.attribute_id.id)])
+                if attr not in self.attribute_line_ids.attribute_id:
+                    self.attribute_line_ids = [
+                        Command.create({
+                                'attribute_id': attr.id,
+                                'product_tmpl_id':self.id,
+                                'value_ids': [Command.link(value.id) for value in line_values]
+                            })
+                        
+                    ]
+                else:
+                    self_line = self.attribute_line_ids.filtered_domain([('attribute_id','=',attr.id)])
+                    add_values = line_values - self_line.value_ids._origin
+                    self_line.value_ids = [
+                        Command.link(value.id) for value in add_values
+                    ]
