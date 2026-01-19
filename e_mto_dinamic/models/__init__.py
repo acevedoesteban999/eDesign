5beef9b3a739a0c2f98fa998d7981672b74494b8
acevedoesteban999 <acevedoesteban999@gmail.com>
2026-01-18 21:57:37 -0500

    [UPD]

--- e_mto_dinamic/models/__init__.py
+++ e_mto_dinamic/models/__init__.py
@@ -2,6 +2,5 @@
 from . import product_template
 from . import product_product
 from . import sale_order_line
-from . import sale_order
 from . import mrp_production
 from . import product_diamic_mto_line
--- e_mto_dinamic/models/mrp_production.py
+++ e_mto_dinamic/models/mrp_production.py
@@ -1,18 +1,32 @@
-from odoo import models
+from odoo import models , api , Command
 
 class MrpProduction(models.Model):
     _inherit = "mrp.production"
     
-    def _get_moves_raw_values(self):
-        moves = []
-        for rec in self:
-            if rec.product_tmpl_id.dinamic_mto_ok:
-                for dinamic_bill in rec.product_tmpl_id.lines_dinamic_bill_material_ids:
-                    moves.append(rec._get_move_raw_values(
-                        dinamic_bill['product_id'],
-                        dinamic_bill['qty'],
-                        rec.product_id.uom_id.id,
-                    ))
-            else:
-                moves += super(MrpProduction,rec)._get_moves_raw_values()
-        return moves
+    def _get_move_dest_line(self,move_finished_ids):
+        for move in move_finished_ids:
+            while move:
+                if move.sale_line_id:
+                    return move.sale_line_id
+                move = move.move_dest_ids[:1]
+
+    @api.model
+    def create(self, values):
+        rec = super().create(values)
+        product = rec.product_id
+        if product.product_tmpl_id.dinamic_mto_ok:
+            moves = []    
+            sale_order_line = self._get_move_dest_line(rec.move_dest_ids)
+            if sale_order_line:
+                for dinamic_bill in sale_order_line.dinamic_bill_material_data:
+                    moves.append(
+                        rec._get_move_raw_values(
+                            self.env['product.product'].browse(dinamic_bill['product']['id']),
+                            dinamic_bill['qty'],
+                            sale_order_line.product_uom
+                        ),
+                    )
+                    
+                rec.move_raw_ids = [Command.create(move) for move in moves]
+        
+        return rec
