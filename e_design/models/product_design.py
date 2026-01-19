37efa4cfdbcd8468f31b42419ea2edf1dd1115f5
acevedoesteban999 <acevedoesteban999@gmail.com>
2026-01-19 14:30:16 -0500

    [UPD] get_formview_action now in e_design Module

--- e_design/models/product_design.py
+++ e_design/models/product_design.py
@@ -166,3 +166,16 @@ class ProductDesign(models.Model):
             attr_value.product_design_id = record.id
             return record
         return  super().create(vals_list)
+    
+    
+    def get_formview_action(self, access_uid=None):
+        view_id = self.sudo().get_formview_id(access_uid=access_uid)
+        
+        return {
+            'type': 'ir.actions.act_window',
+            'res_model': self._name,
+            'views': [(view_id, 'form')],
+            'target': self._context.get('action_target') or 'current' ,
+            'res_id': self.id,
+            'context': dict(self._context),
+        }
