705682b96929294d1ff9548d3848f14f68d47602
acevedoesteban999 <acevedoesteban999@gmail.com>
2026-01-28 18:08:22 -0500

    [UPD] README

--- e_design_mrp_pos/__manifest__.py
+++ e_design_mrp_pos/__manifest__.py
@@ -5,15 +5,13 @@
     'summary': """Integration POS-MRP with eDesign""",
     'description':"""
                     It allows you to view the designs of the product 
-                    to be manufactured in the manufacturing orders.
+                    to be manufactured from POS in the manufacturing orders.
                 """,
     'author': 'acevedoesteban999@gmail.com',
-    'website': 'https://github.com/acevedoesteban999/eAddons/blob/18.0/e_design_pos_mrp',
+    'website': 'https://github.com/acevedoesteban999/eAddons/blob/18.0/e_design_mrp_pos',
     'category': 'Customizations',
     'depends': ['e_design_mrp','e_design_pos','e_pos_mrp'],
-    "data": [
-        
-    ],
+    "data": [ ],
     'images': [
         'static/description/banner.png',
     ],
