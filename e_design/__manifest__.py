e11e4eca39ce079cac0a9099b62fa916b7e0e829
acevedoesteban999 <acevedoesteban999@gmail.com>
2026-02-11 12:44:02 -0500

    [UPD] e_design: searchs

--- e_design/__manifest__.py
+++ e_design/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'eDesign',
-    'version': '18.0.3.0.3',
+    'version': '18.0.3.1.0',
     'summary': "Add Design to Products",
     'description':"""
                     It allows you to add designs to products, 
